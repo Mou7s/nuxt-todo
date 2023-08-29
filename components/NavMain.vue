d99294a50eb6fb105c91c8181a68e0c5d5b6268a
mou7s <liuchuge7777777@gmail.com>
2023-08-29 12:54:42 +0800

    added group tailwindcss

--- components/NavMain.vue
+++ components/NavMain.vue
@@ -16,11 +16,11 @@ let list = ref([
 </script>
 
 <template>
-  <div v-for="(item, index) in list" :key="index">
-    <div>
-      <input type="checkbox" name="" id="" v-model="item.completed" />
-      {{ item.title }}
-      <button>delete</button>
-    </div>
+  <div v-for="(item, index) in list" :key="index" class="relative group">
+    <input type="checkbox" v-model="item.completed" />
+    {{ item.title }}
+    <button class="absolute right-1 opacity-0 group-hover:opacity-100">
+      delete
+    </button>
   </div>
 </template>
