e5001d793938276886b8bb2e422501111241495e
mou7s <liuchuge7777777@gmail.com>
2023-08-29 09:27:01 +0800

    #

---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="min-h-screen bg-white dark:bg-neutral-800 grid place-content-center">
+    <p class="text-9xl font-bold dark:text-neutral-50">hello world!</p>
+  </div>
+</template>
+
+
+
+
+<style scoped></style>
