a57a9387b64ff9c5cccab976bd5e471a4b1b7445
mou7s <liuchuge7777777@gmail.com>
2023-08-31 00:34:41 +0800

    #

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,40 @@
 <template>
-  <div
-    class="grid place-content-center min-h-screen dark:bg-neutral-950 dark:text-neutral-50"
-  >
-    <section>
-      <header class="header">
-        <h1>todos</h1>
-        <input
-          class="new-todo"
-          autofocus
-          autocomplete="off"
-          placeholder="What needs to be done?"
-          v-model="newTodo"
-          @keydown.enter="addTodo"
-        />
-      </header>
-    </section>
+  <div class="p-10 min-h-screen dark:bg-neutral-900 dark:text-neutral-50">
+    <div class="grid place-content-center w-96 min-w-full">
+      <h1 class="text-8xl">todos</h1>
+      <input
+        v-model="newTodo"
+        @keyup.enter="addTodo"
+        placeholder="What needs to be done?"
+        class="h-12 rounded w"
+      />
+      <ul>
+        <li v-for="(todo, index) in todos" :key="index" class="relative group">
+          <input type="checkbox" v-model="todo.completed" />
+          {{ todo.title }}
+          <button
+            @click="removeTodo(index)"
+            class="right-0 absolute opacity-0 group-hover:opacity-100"
+          >
+            Remove
+          </button>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-onMounted(() => {
-  todos.value = todoStorage.fetch();
-});
-
-// app initial state
-var STORAGE_KEY = 'todos-vuejs';
-let todoStorage = {
-  fetch: function () {
-    return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
-  },
-  save: function (todos: object) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
-  },
-};
-
-const todos = ref(todoStorage.fetch());
 const newTodo = ref('');
-
-// watch todos change for localStorage persistence
-watch(
-  todos,
-  (newTodos) => {
-    todoStorage.save(newTodos);
-  },
-  { deep: true }
-);
+const todos = ref([]);
 
 const addTodo = () => {
-  const value = newTodo.value.trim();
-  if (!value) {
-    return;
-  }
-  // TODO: Use a proper UUID instead of `Date.now()`.
-  todos.value.push({ id: Date.now(), title: value, completed: false });
+  if (newTodo.value.trim() === '') return;
+  todos.value.push({ title: newTodo.value, completed: false });
   newTodo.value = '';
+};
+
+const removeTodo = (index: number) => {
+  todos.value.splice(index, 1);
 };
 </script>
