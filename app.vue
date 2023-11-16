870f0bf5b43d2f04357a7bbb80eed823f884c242
mou7s <liuchuge7777777@gmail.com>
2023-11-16 11:53:06 +0800

    #

---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup>
+const todos = ref([]);
+
+const addTodo = (todo) => {
+  todos.value.push(todo);
+};
+</script>
+
+<template>
+  <UContainer>
+    <UCard class="mt-10"> <p class="font-bold text-center">ongoing</p></UCard>
+    <UCard class="mt-10"> <p class="font-bold text-center">done</p></UCard>
+  </UContainer>
+</template>
