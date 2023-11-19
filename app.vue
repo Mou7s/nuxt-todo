5e1e8f96376dc45641d0464c85a4c3ee0d631ea1
mou7s <liuchuge7777777@gmail.com>
2023-11-19 23:48:16 +0800

    #

--- app.vue
+++ app.vue
@@ -71,6 +71,10 @@ const clearLocalStorage = () => {
 </script>
 
 <template>
+  <meta
+    name="google-site-verification"
+    content="uMb53AX2NHoWvhBjQGUPiU2EvzBs2pWg90HYPMkcMP4"
+  />
   <UContainer>
     <p class="text-center text-8xl font-serif">todos</p>
 
