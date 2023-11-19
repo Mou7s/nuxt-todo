4fed7a3702358317d55a1035b1ad8a1a7da9a100
mou7s <liuchuge7777777@gmail.com>
2023-11-19 13:50:31 +0800

    Update app.vue

--- app.vue
+++ app.vue
@@ -17,11 +17,22 @@ const addTodo = () => {
     todos.value.push(todo.value);
     todo.value = '';
   }
+
+  updateLocalStorage();
 };
 
 const doneTodo = (index) => {
+  // 将 'todos' 中的值移动到 'dones'
   dones.value.push(todos.value[index]);
+  // 从 'todos' 中删除特定索引的值
   todos.value.splice(index, 1);
+
+  updateLocalStorage();
+};
+
+const updateLocalStorage = () => {
+  localStorage.setItem('todos', JSON.stringify(todos.value));
+  localStorage.setItem('dones', JSON.stringify(dones.value));
 };
 </script>
 
@@ -40,7 +51,7 @@ const doneTodo = (index) => {
     <UCard class="mt-10">
       <p class="font-bold text-center">ongoing</p>
 
-      <div v-for="(todo, index) of todos" class="text-center">
+      <div v-for="(todo, index) of todos" :key="index" class="text-center">
         <p class="m-2">
           {{ todo }} <UButton @click="doneTodo(index)">Del</UButton>
         </p>
@@ -49,7 +60,7 @@ const doneTodo = (index) => {
     <UCard class="mt-10">
       <p class="font-bold text-center">done</p>
 
-      <div v-for="(done, index) of dones" class="text-center">
+      <div v-for="(done, index) of dones" :key="index" class="text-center">
         <p class="m-2">
           {{ done }}
         </p>
